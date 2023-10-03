e6a8dae4a78aa4d101cdd5178e81f123af6ec839
abc-valera <zoocity14@gmail.com>
2023-10-03 19:00:05 +0300

    REFACTOR: moved openapi spec

--- tools/ogen/generate.go
+++ tools/ogen/generate.go
@@ -1,3 +1,3 @@
 package ogen
 
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen@latest --convenient-errors --package ogen --target . --clean ../../openapi.yml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen@latest --convenient-errors --package ogen --target . --clean ../../internal/infrastructure/port/http/schema/openapi.yml
