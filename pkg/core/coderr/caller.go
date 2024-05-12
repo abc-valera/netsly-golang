61bf477260688f2fe4fdd39a4b6dcac146309d50
abc-valera <zoocity14@gmail.com>
2024-05-12 22:28:30 +0300

    FIX: fixed bug with coderr

--- pkg/core/coderr/caller.go
+++ pkg/core/coderr/caller.go
@@ -10,6 +10,16 @@ import (
 func caller(skip int) string {
 	// Get the caller
 	_, file, line, _ := runtime.Caller(skip)
-	file = strings.Split(file, "netsly-golang/")[1]
-	return fmt.Sprintf("[%s:%d]", file, line)
+
+	var msg string
+
+	// Split the file path
+	split := strings.Split(file, "bettermill-accra/")
+	if len(split) == 1 {
+		msg = fmt.Sprintf("[%s:%d]", file, line)
+	} else {
+		msg = fmt.Sprintf("[%s:%d]", split[1], line)
+	}
+
+	return msg
 }
